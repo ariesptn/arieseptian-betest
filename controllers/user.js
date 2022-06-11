581efce9af2397e837a23239a48a9cb2afcd3834
Arie Septian <arie.septian@alodokter.com>
2022-06-11 20:44:11 +0700

    added redis cache

--- controllers/user.js
+++ controllers/user.js
@@ -8,37 +8,42 @@ class UserController {
             const userData = await models.User.find({}).lean()
             res.status(200).json({ userData })
         } catch (err) {
-            console.log(err)
             res.status(500).json({error: err.toString()})
         }
     }
 
     static async findById(req, res) {
         try {
+            const cachedUserData = await redisClient.get(`user|id|${req.params.id}`)
+            if(cachedUserData) return res.status(200).json({ userData: JSON.parse(cachedUserData) })
             const userData = await models.User.findById(req.params.id).lean()
+            await redisClient.set(`user|id|${req.params.id}`, JSON.stringify(userData), {EX: 86400})
             res.status(200).json({ userData })
         } catch (err) {
-            console.log(err)
             res.status(500).json({error: err.toString()})
         }
     }
 
     static async findByAccountNumber(req, res) {
         try {
+            const cachedUserData = await redisClient.get(`user|accountNUmber|${req.query.account_number}`)
+            if(cachedUserData) return res.status(200).json({ userData: JSON.parse(cachedUserData) })
             const userData = await models.User.findOne({accountNumber: req.query.account_number}).lean()
+            await redisClient.set(`user|accountNUmber|${req.query.account_number}`, JSON.stringify(userData), {EX: 86400})
             res.status(200).json({ userData })
         } catch (err) {
-            console.log(err)
             res.status(500).json({error: err.toString()})
         }
     }
 
     static async findByIdentityNumber(req, res) {
         try {
+            const cachedUserData = await redisClient.get(`user|identityNumber|${req.query.identity_number}`)
+            if(cachedUserData) return res.status(200).json({ userData: JSON.parse(cachedUserData) })
             const userData = await models.User.findOne({identityNumber: req.query.identity_number}).lean()
+            await redisClient.set(`user|identityNumber|${req.query.identity_number}`, JSON.stringify(userData), {EX: 86400})
             res.status(200).json({ userData })
         } catch (err) {
-            console.log(err)
             res.status(500).json({error: err.toString()})
         }
     }
@@ -54,14 +59,17 @@ class UserController {
             userData = await models.User.findById(userData._id).lean()
             res.status(200).json({ userData })
         } catch (err) {
-            console.log(err)
             res.status(500).json({error: err.toString()})
         }
     }
 
     static async edit(req, res) {
         try {
-            let userData = await models.User.findOneAndUpdate(
+            let userData = await models.User.findById(req.params.id).lean()
+            await redisClient.del(`user|id|${req.params.id}`)
+            await redisClient.del(`user|identityNumber|${userData.identityNumber}`)
+            await redisClient.del(`user|accountNUmber|${userData.accountNumber}`)
+            userData = await models.User.findOneAndUpdate(
                 {
                     _id: req.params.id,
                 },
@@ -74,17 +82,19 @@ class UserController {
             userData = await models.User.findById(userData._id).lean()
             res.status(200).json({ userData })
         } catch (err) {
-            console.log(err)
             res.status(500).json({error: err.toString()})
         }
     }
 
     static async remove(req, res) {
         try {
-            const userData = await models.User.deleteOne({_id: req.params.id})
+            let userData = await models.User.findById(req.params.id).lean()
+            await redisClient.del(`user|id|${req.params.id}`)
+            await redisClient.del(`user|identityNumber|${userData.identityNumber}`)
+            await redisClient.del(`user|accountNUmber|${userData.accountNumber}`)
+            userData = await models.User.deleteOne({_id: req.params.id})
             res.status(200).json({ userData })
         } catch (err) {
-            console.log(err)
             res.status(500).json({error: err.toString()})
         }
     }
@@ -93,10 +103,9 @@ class UserController {
         try {
             const tokenNum = Math.random().toString()
             const token = await getToken({token: tokenNum})
-            await redisClient.set(`authToken_${tokenNum}`, 'true')
+            await redisClient.set(`authToken|${tokenNum}`, 'true', {EX: 86400})
             res.status(200).json({ token })
         } catch (err) {
-            console.log(err)
             res.status(500).json({error: err.toString()})
         }
     }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,18 +6,17 @@ async function authentication(req, res, next) {
         const authorizationHeader = req.headers.authorization ? req.headers.authorization.replace('Bearer ','') : ''
         let decoded = jwt.verify(authorizationHeader, process.env.JWT_SECRET)
         if (decoded) {
-            const sessionStatus = await redisClient.get(`authToken_${decoded.token}`)
+            const sessionStatus = await redisClient.get(`authToken|${decoded.token}`)
             if(sessionStatus === 'true'){
                 next()
             } else {
-                throw new Error('Token is invalid or expired')
+                res.status(401).json({error: 'Token is invalid or expired'})
             }
         } else {
-            throw new Error('Token is invalid')
+            res.status(401).json({error: 'Token is invalid'})
         }
     } catch (err) {
         res.status(401).json({error: err.toString()})
-        console.log(err)
     }
 }
 
